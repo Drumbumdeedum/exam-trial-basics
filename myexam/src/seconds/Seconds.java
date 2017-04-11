dd676f5c4c265b9ae617c0f25a91a6e51f6bfee9
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-11 15:27:58 +0200

    add seconds excersise

--- myexam/src/seconds/Seconds.java
+++ myexam/src/seconds/Seconds.java
@@ -1,9 +1,26 @@
 package seconds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seconds {
 
   public static void main(String[] args) {
-
+    List<Integer> newList = new ArrayList();
+    for (int i=0; i < 10; i++) {
+      newList.add(i+1);
+    }
+    System.out.println("Original list: " + newList);
+    System.out.println("Every second member of original list: " + getEverySecond(newList));
   }
 
+  public static List<Integer> getEverySecond(List<Integer> inputList) {
+    List<Integer> secondsList = new ArrayList<>();
+
+    for (int i=0; i < inputList.size(); i += 2) {
+      secondsList.add(inputList.get(i));
+    }
+
+    return secondsList;
+  }
 }
